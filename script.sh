8f012c167f615c6a768cf0d63f77bdf9a89a5b9b
document10 <popescul2222georgeteodor@gmail.com>
2020-12-01 22:37:17 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,12 @@
-echo --A2DP sink problems--
+echo Note this only works on debian based dirtros.
+echo --Bluetooth not avaible on wake problem--
+echo Current bluez version
+echo Updating dependencies...
+sudo add-apt-repository ppa:bluetooth/bluez
+sudo apt-get update
+sudo apt upgrade
+dpkg --status bluez | grep '^Version:'
+echo --A2DP sink default problem--
 echo The sccript will need to install some dependencies or update them if they are already installed.
 sudo apt install pulseaudio pulseaudio-utils pavucontrol pulseaudio-module-bluetooth
 echo A file will opened in your text editor.If you haven't alredy,type these lines:
