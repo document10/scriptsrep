fc62c8958c2ede88ec869dbf032c0a134c3c6707
document10 <popescul2222georgeteodor@gmail.com>
2021-08-06 22:52:49 +0300

    Update arch-install-uefi.sh

--- arch-install-uefi.sh
+++ arch-install-uefi.sh
@@ -40,7 +40,7 @@ echo "Loading swapfile"
 echo "/swapfile	none	swap	defaults	0	0" >> /etc/fstab
 cat /etc/fstab
 echo "kernel.sysrq = 1" >> /etc/sysctl.d/99.sysctl.conf
-echo "vm.vfs_cache_pressure=50" >> /etc/sysctl.d/99.sysctl.conf
+echo "vm.vfs_cache_pressure = 50" >> /etc/sysctl.d/99.sysctl.conf
 echo "vm.swappiness = 5" >> /etc/sysctl.d/99.sysctl.conf
 echo "Swapfile settings"
 cat /etc/sysctl.d/99.sysctl.conf
