f356a5234411267f172843e1daff83880f3f27a1
document10 <popescul2222georgeteodor@gmail.com>
2021-08-07 23:44:18 +0300

    Update arch-install-uefi.sh
    Separated swapfile part from the installation script

--- arch-install-uefi.sh
+++ arch-install-uefi.sh
@@ -30,20 +30,6 @@ echo "Password for doc10:"
 passwd doc10
 echo "doc10 ALL=(ALL) ALL" >> /etc/sudoers.d/doc10
 
-#creating a swapfile
-echo "Creating swapfile"
-fallocate -l 1024M /swapfile
-chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
-echo "Loading swapfile"
-echo "/swapfile	none	swap	defaults	0	0" >> /etc/fstab
-cat /etc/fstab
-echo "kernel.sysrq = 1" >> /etc/sysctl.d/99.sysctl.conf
-echo "vm.vfs_cache_pressure = 50" >> /etc/sysctl.d/99.sysctl.conf
-echo "vm.swappiness = 5" >> /etc/sysctl.d/99.sysctl.conf
-echo "Swapfile settings"
-cat /etc/sysctl.d/99.sysctl.conf
 echo "Final configuration"
 mkinitcpio -P
 free -m
