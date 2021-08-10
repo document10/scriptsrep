607f2e7bac8edead2f689e4de815ec42ce60f59e
document10 <popescul2222georgeteodor@gmail.com>
2021-08-11 00:07:16 +0300

    Update install_dwm.sh

--- install_dwm.sh
+++ install_dwm.sh
@@ -24,14 +24,18 @@ cd surf
 make
 sudo make install
 cd
-sudo mkdir -p /usr/share/xsessions
-sudo echo "[Desktop Entry]" >> /usr/share/xsessions/dwm.desktop
-sudo echo "Encoding=UTF-8" >> /usr/share/xsessions/dwm.desktop
-sudo echo "Name=Dwm" >> /usr/share/xsessions/dwm.desktop
-sudo echo "Comment=Dynamyc window manager" >> /usr/share/xsessions/dwm.desktop
-sudo echo "Exec=dwm" >> /usr/share/xsessions/dwm.desktop
-sudo echo "Icon=dwm" >> /usr/share/xsessions/dwm.desktop
-sudo echo "Type=XSession" >> /usr/share/xsessions/dwm.desktop
+echo "Entering root user"
+su
+mkdir -p /usr/share/xsessions
+echo "[Desktop Entry]" >> /usr/share/xsessions/dwm.desktop
+echo "Encoding=UTF-8" >> /usr/share/xsessions/dwm.desktop
+echo "Name=Dwm" >> /usr/share/xsessions/dwm.desktop
+echo "Comment=Dynamyc window manager" >> /usr/share/xsessions/dwm.desktop
+echo "Exec=dwm" >> /usr/share/xsessions/dwm.desktop
+echo "Icon=dwm" >> /usr/share/xsessions/dwm.desktop
+echo "Type=XSession" >> /usr/share/xsessions/dwm.desktop
+exit
+echo "Exited root user"
 echo "nitrogen --restore &" >> .xprofile
 echo "picom -f &" >>.xprofile
 sudo systemctl enable lightdm
