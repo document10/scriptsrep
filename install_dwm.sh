a890ae36be6d1d3a8fe344e5be1a26b73d1b65f6
document10 <popescul2222georgeteodor@gmail.com>
2021-08-13 17:59:03 +0300

    Update install_dwm.sh

--- install_dwm.sh
+++ install_dwm.sh
@@ -36,6 +36,6 @@ echo "Comment=Dynamyc window manager" >> /usr/share/xsessions/dwm.desktop
 echo "Exec=dwm" >> /usr/share/xsessions/dwm.desktop
 echo "Icon=dwm" >> /usr/share/xsessions/dwm.desktop
 echo "Type=XSession" >> /usr/share/xsessions/dwm.desktop
-echo "nitrogen --restore &" >> .xprofile
-echo "picom -f &" >>.xprofile
+echo "nitrogen --restore &" >> /home/doc10/.xprofile
+echo "picom -f &" >>/home/doc10/.xprofile
 systemctl enable lightdm
