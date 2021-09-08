48b91f065a4b9cc26200a14547ec6836622c87be
document10 <popescul2222georgeteodor@gmail.com>
2021-09-08 15:28:45 +0300

    Update install_xfce.sh

--- install_xfce.sh
+++ install_xfce.sh
@@ -1,1 +1,5 @@
-#under construction
+#must run as root
+pacman -Syu
+pacman -S xorg xorg-server xterm firefox lightdm  lightdm-gtk-greeter lightdm-gtk-greeter-settings archlinux-wallpaper avahi xdg-user-dirs xdg-utils gedit bluez bluez-utils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack sof-firmware xfce4 xfce4-goodies
+systemctl enable lightdm
+systemctl enable bluetooth
