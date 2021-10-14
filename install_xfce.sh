651fbffa1d511962e6fa67961d4857d98b915ce6
document10 <popescul2222georgeteodor@gmail.com>
2021-10-14 20:53:00 +0300

    Update install_xfce.sh

--- install_xfce.sh
+++ install_xfce.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #must run as root
 pacman -Syu
-pacman -S xorg xorg-server xterm firefox lightdm  lightdm-gtk-greeter lightdm-gtk-greeter-settings archlinux-wallpaper avahi xdg-user-dirs xdg-utils gedit bluez bluez-utils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack sof-firmware xfce4 xfce4-goodies blueman
+pacman -S xorg xorg-server xterm firefox lightdm  lightdm-gtk-greeter lightdm-gtk-greeter-settings archlinux-wallpaper avahi xdg-user-dirs xdg-utils gedit bluez bluez-utils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack sof-firmware xfce4 xfce4-goodies blueman arc-solid-gtk-theme arc-gtk-theme arc-icon-theme
 #gpu drivers,COMMENT THE ONES YOU DON'T NEED
 pacman -S  xf86-video-amdgpu
 pacman -S  nvidia nvidia-utils nvidia-settings
