33d535bb8985890c7e76ca4a9920ec1470706b49
document10 <popescul2222georgeteodor@gmail.com>
2021-11-06 17:22:07 +0200

    Update dwm-freebsd.sh

--- dwm-freebsd.sh
+++ dwm-freebsd.sh
@@ -1,3 +1,5 @@
-pkg install xorg dwm lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings nitrogen
+pkg install xorg dwm lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings nitrogen pcmanfm compton
 sysrc lightdm_enable=YES
+echo "nitrogen --restore &" >> /home/dxvk/.xprofile
+echo "picom -f &" >>/home/dxvk/.xprofile
 #THAT'S ALL FOLKS!!!
