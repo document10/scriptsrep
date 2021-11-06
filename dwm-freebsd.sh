33bbccbe3320399599c1d9a16e65c4bea02f4bc7
document10 <popescul2222georgeteodor@gmail.com>
2021-11-06 17:11:39 +0200

    Update dwm-freebsd.sh

--- dwm-freebsd.sh
+++ dwm-freebsd.sh
@@ -1,3 +1,3 @@
-pkg install xorg dwm lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings 
+pkg install xorg dwm lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings nitrogen
 sysrc lightdm_enable=YES
 #THAT'S ALL FOLKS!!!
