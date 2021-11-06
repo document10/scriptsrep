b210bcd28c683bc919d1d82d61f96cef1ab445b6
document10 <popescul2222georgeteodor@gmail.com>
2021-11-06 18:25:12 +0200

    Update dwm-freebsd.sh

--- dwm-freebsd.sh
+++ dwm-freebsd.sh
@@ -1,5 +1,5 @@
 pkg install xorg dwm lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings nitrogen pcmanfm compton xdg-user-dirs xdg-utils gedit gnome-backgrounds oss polkit polkit-gnome
-echo 'cuse_load=yes' >> /boot/loader.conf
+echo 'cuse_load="YES" ' >> /boot/loader.conf
 sysrc lightdm_enable=YES
 echo "nitrogen --restore &" >> /home/dxvk/.xprofile
 echo "compton -f &" >>/home/dxvk/.xprofile
