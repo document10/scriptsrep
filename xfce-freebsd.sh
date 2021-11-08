fae4018836920c965e8ec88d8e99148ada34d7f4
document10 <popescul2222georgeteodor@gmail.com>
2021-11-08 23:28:52 +0200

    Update xfce-freebsd.sh

--- xfce-freebsd.sh
+++ xfce-freebsd.sh
@@ -1,3 +1,3 @@
-pkg install xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xdg-user-dirs xdg-utils oss polkit polkit-gnome xfce
+pkg install xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xdg-user-dirs xdg-utils oss polkit polkit-gnome xfce gtk-arc-themes
 sysrc lightdm_enable=YES
 #THAT'S ALL FOLKS!!!
