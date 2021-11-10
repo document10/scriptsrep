ec836c9051881d70ccbfdfc6dabd36a259ae645c
document10 <popescul2222georgeteodor@gmail.com>
2021-11-10 23:11:15 +0200

    Update freebsd-setup.sh

--- freebsd-setup.sh
+++ freebsd-setup.sh
@@ -9,7 +9,7 @@ freebsd-update fetch
 freebsd-update install
 pkg bootstrap
 pkg update && pkg upgrade
-pkg install -y sudo nano git bash neofetch
+pkg install -y sudo nano git bash neofetch base gcc
 sysrc dbus_enable=YES
 sysrc hald_enable=YES
 sysrc sound_load=YES
