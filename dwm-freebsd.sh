6b05ad5ac13cd8e1e4dcb80828b86b26f2549c74
document10 <popescul2222georgeteodor@gmail.com>
2021-11-10 22:59:47 +0200

    Update dwm-freebsd.sh

--- dwm-freebsd.sh
+++ dwm-freebsd.sh
@@ -1,6 +1,7 @@
-pkg install xorg dwm lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings nitrogen pcmanfm compton xdg-user-dirs xdg-utils gedit gnome-backgrounds oss polkit polkit-gnome gtk-arc-themes
+pkg install xorg dwm xf86-input-evdev lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings nitrogen pcmanfm compton xdg-user-dirs xdg-utils gedit gnome-backgrounds oss polkit polkit-gnome gtk-arc-themes
 sysrc lightdm_enable=YES
 echo "nitrogen --restore &" >> /home/dxvk/.xprofile
 echo "compton -f &" >>/home/dxvk/.xprofile
 sysrc compton_enable="YES"
+sysrc webcamd_enable="YES"
 #THAT'S ALL FOLKS!!!
