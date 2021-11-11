983f8710e03cbd8817bd1d85c8e7fcc147fa7705
document10 <popescul2222georgeteodor@gmail.com>
2021-11-11 11:49:13 +0200

    Update openbox-freebsd.sh

--- openbox-freebsd.sh
+++ openbox-freebsd.sh
@@ -1,9 +1,8 @@
-pkg install xf86-input-evdev xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings nitrogen pcmanfm compton xdg-user-dirs xdg-utils gedit mousepad gnome-backgrounds oss polkit polkit-gnome gtk-arc-themes openbox obconf tint2 menumaker clearlooks
+pkg install xf86-input-evdev xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings pcmanfm compton xdg-user-dirs xdg-utils gedit mousepad gnome-backgrounds oss polkit polkit-gnome gtk-arc-themes openbox obconf tint2 menumaker clearlooks
 mmaker openbox -f -t xterm
 sysrc lightdm_enable=YES
 echo "mmaker openbox -f -t xterm &" >> /home/dxvk/.xprofile
 echo "openbox --reconfigure" >> /home/dxvk/.xprofile
-echo "nitrogen --restore &" >> /home/dxvk/.xprofile
 echo "compton -f &" >> /home/dxvk/.xprofile
 echo "tint2 &" >> /home/dxvk/.xprofile
 echo "pcmanfm --desktop" >> /home/dxvk/.xprofile
