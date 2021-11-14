e4d485849f6e99e4fa14dd1f0ac00a8dc7ef9cfd
document10 <popescul2222georgeteodor@gmail.com>
2021-11-14 13:21:41 +0200

    Update openbox-freebsd.sh

--- openbox-freebsd.sh
+++ openbox-freebsd.sh
@@ -1,4 +1,4 @@
-pkg install xf86-input-evdev xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings pcmanfm compton xdg-user-dirs xdg-utils mousepad gnome-backgrounds oss polkit polkit-gnome gtk-arc-themes openbox obconf tint2 menumaker clearlooks dmenu
+pkg install xf86-input-evdev xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings pcmanfm compton xdg-user-dirs xdg-utils mousepad gnome-backgrounds oss polkit polkit-gnome gtk-arc-themes openbox obconf tint2 menumaker clearlooks dmenu volumeicon
 mmaker openbox -f -t xterm
 sysrc lightdm_enable=YES
 echo "mmaker openbox -f -t xfterm &" >> /home/dxvk/.xprofile
@@ -6,6 +6,6 @@ echo "openbox --reconfigure" >> /home/dxvk/.xprofile
 echo "compton -f &" >> /home/dxvk/.xprofile
 echo "tint2 &" >> /home/dxvk/.xprofile
 echo "pcmanfm -d --desktop" >> /home/dxvk/.xprofile
+echo "volumeicon" >> /home/dxvk/.xprofile
 sysrc compton_enable="YES"
 sysrc webcamd_enable="YES"
-#THAT'S ALL FOLKS!!!
