0a7e5089ac6d4b67a8880b66e3cda6a3a31b0266
document10 <popescul2222georgeteodor@gmail.com>
2021-11-20 14:36:59 +0200

    Update install_paru.sh

--- install_paru.sh
+++ install_paru.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #DO NOT RUN AS ROOT
-sudo pacman -S --needed base-devel
+echo "Installing required dependencies (just in case)"
+sudo pacman -Syu --noconfirm base-devel git
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
+paru -Qua
+echo "Paru has been installed succesfully."
