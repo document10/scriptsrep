d60ecc6925ee429eb07c5b5b64450cbc940a56fd
document10 <popescul2222georgeteodor@gmail.com>
2021-11-29 20:57:47 +0200

    Update mate-freebsd.sh

--- mate-freebsd.sh
+++ mate-freebsd.sh
@@ -1,5 +1,4 @@
-pkg install -y xf86-input-evdev xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xdg-user-dirs xdg-utils oss polkit polkit-gnome gtk-arc-themes
-pkg install -g 'mate-'
+pkg install -y xf86-input-evdev xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xdg-user-dirs xdg-utils oss polkit polkit-gnome gtk-arc-themes mate
 sysrc lightdm_enable=YES
 sysrc webcamd_enable=YES
 #THAT'S ALL FOLKS!!!
