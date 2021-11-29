eb9de67158145414cab599588ca1fb0f7d98df91
document10 <popescul2222georgeteodor@gmail.com>
2021-11-29 22:22:54 +0200

    Update lxqt-freebsd.sh

--- lxqt-freebsd.sh
+++ lxqt-freebsd.sh
@@ -1,4 +1,6 @@
-pkg install -y xf86-input-evdev xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xdg-user-dirs xdg-utils oss polkit polkit-gnome gtk-arc-themes lxqt bsdisks
+pkg install -y xf86-input-evdev xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xdg-user-dirs xdg-utils polkit polkit-gnome gtk-arc-themes lxqt bsdisks compton
 sysrc lightdm_enable=YES
 sysrc webcamd_enable=YES
+echo "compton -f &" >> /home/dxvk/.xprofile
+echo "lxqt-panel &" >> /home/dxvk/.xprofile
 #THAT'S ALL FOLKS!!!
