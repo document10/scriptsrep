8ec77ebd67b602a4d12f5b54b3441774f05cf05d
document10 <popescul2222georgeteodor@gmail.com>
2021-11-29 23:28:22 +0200

    Create lxqt-arch.sh

---
+++ lxqt-arch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#must run as root
+pacman -Syu
+pacman -S xorg xorg-server xterm firefox lightdm  lightdm-gtk-greeter lightdm-gtk-greeter-settings archlinux-wallpaper avahi xdg-user-dirs xdg-utils gedit bluez bluez-utils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack sof-firmware lxqt xdg-utils ttf-freefont blueman arc-solid-gtk-theme arc-gtk-theme arc-icon-theme libpulse libstatgrab libsysstat lm_sensors network-manager-applet oxygen-icons pavucontrol-qt picom
+#gpu drivers,COMMENT THE ONES YOU DON'T NEED
+pacman -S  xf86-video-amdgpu
+pacman -S  nvidia nvidia-utils nvidia-settings
+pacman -S virtualbox-guest-utils
+systemctl enable lightdm
+systemctl enable bluetooth
+echo "compton -f &" >> /home/doc10/.xprofile
