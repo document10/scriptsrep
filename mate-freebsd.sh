46ddd50b413c1d591a00a553ee7a31b6baa6b0ec
document10 <popescul2222georgeteodor@gmail.com>
2021-11-29 20:30:37 +0200

    Create mate-freebsd.sh

---
+++ mate-freebsd.sh
@@ -0,0 +1,5 @@
+pkg install -y xf86-input-evdev xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xdg-user-dirs xdg-utils oss polkit polkit-gnome gtk-arc-themes
+pkg install -g 'mate-'
+sysrc lightdm_enable=YES
+sysrc webcamd_enable=YES
+#THAT'S ALL FOLKS!!!
