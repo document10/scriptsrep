f035803067ef54ef134d2a563c6fda4c0e159e0e
document10 <popescul2222georgeteodor@gmail.com>
2021-12-01 18:41:52 +0200

    Update plasma-arch.sh

--- plasma-arch.sh
+++ plasma-arch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #must run as root
 pacman -Syu
-pacman -S xorg xorg-server xterm firefox archlinux-wallpaper avahi xdg-user-dirs xdg-utils gedit bluez bluez-utils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack sof-firmware blueman plasma plasma-wayland-session kde-applications
+pacman -S xorg xorg-server xterm firefox archlinux-wallpaper avahi xdg-user-dirs xdg-utils gedit bluez bluez-utils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack sof-firmware blueman plasma plasma-wayland-session kde-applications packagekit-qt5
 #gpu drivers,COMMENT THE ONES YOU DON'T NEED
 pacman -S  xf86-video-amdgpu
 pacman -S  nvidia nvidia-utils nvidia-settings
