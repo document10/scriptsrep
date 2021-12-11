c15f329f2b3d783c9f324762060366bb6d089ccd
document10 <popescul2222georgeteodor@gmail.com>
2021-12-11 23:52:02 +0200

    Update cinnamon-freebsd.sh

--- cinnamon-freebsd.sh
+++ cinnamon-freebsd.sh
@@ -1,4 +1,4 @@
-pkg install -y xf86-input-evdev xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xdg-user-dirs xdg-utils oss polkit polkit-gnome cinnamon gtk-arc-themes
-sysrc lightdm_enable=YES
+pkg install -y xf86-input-evdev xorg xdg-user-dirs xdg-utils oss polkit polkit-gnome cinnamon gtk-arc-themes
+sysrc gdm_enable="YES"
 sysrc webcamd_enable=YES
 #THAT'S ALL FOLKS!!!
