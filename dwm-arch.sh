96099f7fec592ed0c56ba254d47588fd280c5a5c
document10 <popescul2222georgeteodor@gmail.com>
2022-01-09 14:10:49 +0200

    Update dwm-arch.sh

--- dwm-arch.sh
+++ dwm-arch.sh
@@ -8,7 +8,7 @@ git clone https://git.suckless.org/dmenu
 git clone https://git.suckless.org/sbase/
 git clone https://git.suckless.org/surf
 pacman -Syu
-pacman -S xorg xorg-server xterm firefox nitrogen picom lightdm  lightdm-gtk-greeter lightdm-gtk-greeter-settings archlinux-wallpaper pcmanfm avahi xdg-user-dirs xdg-utils gedit bluez bluez-utils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack sof-firmware blueman arc-solid-gtk-theme arc-gtk-theme arc-icon-theme
+pacman -S xorg xorg-server xterm firefox nitrogen picom lightdm  lightdm-gtk-greeter lightdm-gtk-greeter-settings archlinux-wallpaper pcmanfm avahi xdg-user-dirs xdg-utils gedit bluez bluez-utils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack sof-firmware blueman arc-solid-gtk-theme arc-gtk-theme arc-icon-theme polkit-gnome
 #gpu drivers,COMMENT THE ONES YOU DON'T NEED
 pacman -S  xf86-video-amdgpu
 pacman -S  nvidia nvidia-utils nvidia-settings
