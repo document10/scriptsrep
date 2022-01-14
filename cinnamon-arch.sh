f8e7733eb5e6f26a0ee306a5756a3375b511f69e
document10 <popescul2222georgeteodor@gmail.com>
2022-01-14 11:35:31 +0200

    Update cinnamon-arch.sh

--- cinnamon-arch.sh
+++ cinnamon-arch.sh
@@ -2,6 +2,5 @@
 #must run as root
 pacman -Syu
 pacman -S xorg xorg-server xterm firefox lightdm  lightdm-gtk-greeter lightdm-gtk-greeter-settings archlinux-wallpaper avahi xdg-user-dirs xdg-utils gedit bluez bluez-utils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack sof-firmware cinnamon nemo-fileroller blueman arc-solid-gtk-theme arc-gtk-theme arc-icon-theme pulseaudio pulseaudio-alsa pavucontrol firefox vlc gimp xfburn thunderbird gedit gnome-system-monitor faenza-icon-theme
-#gpu drivers,COMMENT THE ONES YOU DON'T NEED
 systemctl enable lightdm
 systemctl enable bluetooth
