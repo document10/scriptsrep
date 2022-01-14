1e884bfc093bdb1a198cc48308a468da37efae8c
document10 <popescul2222georgeteodor@gmail.com>
2022-01-14 11:27:07 +0200

    Update gnome-arch.sh

--- gnome-arch.sh
+++ gnome-arch.sh
@@ -3,8 +3,9 @@
 pacman -Syu
 pacman -S xorg xorg-server xterm firefox archlinux-wallpaper avahi xdg-user-dirs xdg-utils gedit bluez bluez-utils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack sof-firmware blueman arc-solid-gtk-theme arc-gtk-theme arc-icon-theme gnome
 #gpu drivers,COMMENT THE ONES YOU DON'T NEED
-pacman -S  xf86-video-amdgpu
-pacman -S  nvidia nvidia-utils nvidia-settings
+pacman -S nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader
+pacman -S lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader
+pacman -S lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader
 pacman -S virtualbox-guest-utils
 systemctl enable gdm.service
 systemctl enable bluetooth
