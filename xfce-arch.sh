b311338004f08ff5e6cab7e25ff29e57312e8133
document10 <popescul2222georgeteodor@gmail.com>
2022-01-14 11:33:51 +0200

    Update xfce-arch.sh

--- xfce-arch.sh
+++ xfce-arch.sh
@@ -2,9 +2,6 @@
 #must run as root
 pacman -Syu
 pacman -S xorg xorg-server xterm firefox lightdm  lightdm-gtk-greeter lightdm-gtk-greeter-settings archlinux-wallpaper avahi xdg-user-dirs xdg-utils gedit bluez bluez-utils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack sof-firmware xfce4 xfce4-goodies blueman arc-solid-gtk-theme arc-gtk-theme arc-icon-theme
-#gpu drivers,COMMENT THE ONES YOU DON'T NEED
-pacman -S  xf86-video-amdgpu
-pacman -S  nvidia nvidia-utils nvidia-settings
-pacman -S virtualbox-guest-utils
+#gpu drivers,COMMENT THE ONES YOU DON'T NE
 systemctl enable lightdm
 systemctl enable bluetooth
