3581391f12cb5d7adc5fddad0cf9329c8038737e
document10 <popescul2222georgeteodor@gmail.com>
2022-01-14 11:37:17 +0200

    Update mate-arch.sh

--- mate-arch.sh
+++ mate-arch.sh
@@ -2,10 +2,5 @@
 #must run as root
 pacman -Syu
 pacman -S xorg xorg-server xterm firefox lightdm  lightdm-gtk-greeter lightdm-gtk-greeter-settings archlinux-wallpaper avahi xdg-user-dirs xdg-utils gedit bluez bluez-utils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack sof-firmware mate mate-extra blueman arc-solid-gtk-theme arc-gtk-theme arc-icon-theme
-#gpu drivers,COMMENT THE ONES YOU DON'T NEED
-pacman -S nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader
-pacman -S lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader
-pacman -S lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader
-pacman -S virtualbox-guest-utils
 systemctl enable lightdm
 systemctl enable bluetooth
