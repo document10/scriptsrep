aa94d7b03e415b3444d7a666b214d32e96171b0c
document10 <popescul2222georgeteodor@gmail.com>
2022-01-14 11:36:30 +0200

    Update gnome-arch.sh

--- gnome-arch.sh
+++ gnome-arch.sh
@@ -2,6 +2,5 @@
 #must run as root
 pacman -Syu
 pacman -S xorg xorg-server xterm firefox archlinux-wallpaper avahi xdg-user-dirs xdg-utils gedit bluez bluez-utils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack sof-firmware blueman arc-solid-gtk-theme arc-gtk-theme arc-icon-theme gnome
-#gpu drivers,COMMENT THE ONES YOU DON'T NEED
 systemctl enable gdm.service
 systemctl enable bluetooth
