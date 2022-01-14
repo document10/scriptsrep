df6ac2441cd1ed179b7a335f2c6ab9b0099ebb5e
document10 <popescul2222georgeteodor@gmail.com>
2022-01-14 11:26:38 +0200

    Update cinnamon-arch.sh

--- cinnamon-arch.sh
+++ cinnamon-arch.sh
@@ -3,8 +3,9 @@
 pacman -Syu
 pacman -S xorg xorg-server xterm firefox lightdm  lightdm-gtk-greeter lightdm-gtk-greeter-settings archlinux-wallpaper avahi xdg-user-dirs xdg-utils gedit bluez bluez-utils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack sof-firmware cinnamon nemo-fileroller blueman arc-solid-gtk-theme arc-gtk-theme arc-icon-theme pulseaudio pulseaudio-alsa pavucontrol firefox vlc gimp xfburn thunderbird gedit gnome-system-monitor faenza-icon-theme
 #gpu drivers,COMMENT THE ONES YOU DON'T NEED
-pacman -S  xf86-video-amdgpu
-pacman -S  nvidia nvidia-utils nvidia-settings
+pacman -S nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader
+pacman -S lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader
+pacman -S lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader
 pacman -S virtualbox-guest-utils
 systemctl enable lightdm
 systemctl enable bluetooth
