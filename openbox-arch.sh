fe1a6ac9ef08af8c8bbbe7f7ff76269c3d70d79f
document10 <popescul2222georgeteodor@gmail.com>
2022-01-14 11:35:10 +0200

    Update openbox-arch.sh

--- openbox-arch.sh
+++ openbox-arch.sh
@@ -3,7 +3,6 @@
 pacman -Syu
 pacman -S xorg xorg-server xterm firefox lightdm  lightdm-gtk-greeter lightdm-gtk-greeter-settings archlinux-wallpaper pcmanfm avahi xdg-user-dirs xdg-utils gedit bluez bluez-utils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack sof-firmware blueman arc-solid-gtk-theme arc-gtk-theme arc-icon-theme openbox obconf tint2 picom lxappearance xarchiver unzip gzip lzip menumaker volumeicon mousepad xfce4-terminal polkit-gnome
 mmaker openbox -f -t xfterm
-#gpu drivers,COMMENT THE ONES YOU DON'T NEED
 systemctl enable lightdm
 systemctl enable bluetooth
 echo "mmaker openbox -f -t xfterm &" >> /home/doc10/.xprofile
