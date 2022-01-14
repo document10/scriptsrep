e6e3d87f5897cbea0085546f11e60851db21e79b
document10 <popescul2222georgeteodor@gmail.com>
2022-01-14 11:37:46 +0200

    Update plasma-arch.sh

--- plasma-arch.sh
+++ plasma-arch.sh
@@ -2,6 +2,5 @@
 #must run as root
 pacman -Syu
 pacman -S xorg xorg-server xterm firefox archlinux-wallpaper avahi xdg-user-dirs xdg-utils gedit bluez bluez-utils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack sof-firmware blueman plasma plasma-wayland-session kde-applications packagekit-qt5
-#gpu drivers,COMMENT THE ONES YOU DON'T NEED
 systemctl enable sddm.service
 systemctl enable bluetooth
