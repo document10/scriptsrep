4f32f2ebf9a889d63f2a7502af4eb6f3627d2b2d
document10 <popescul2222georgeteodor@gmail.com>
2022-02-20 21:45:28 +0200

    Create lxde-freebsd.sh

---
+++ lxde-freebsd.sh
@@ -0,0 +1,6 @@
+pkg install -y xf86-input-evdev xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xdg-user-dirs xdg-utils polkit polkit-gnome gtk-arc-themes lxde-meta bsdisks picom
+sysrc lightdm_enable=YES
+sysrc webcamd_enable=YES
+echo "compton -f &" >> /home/dxvk/.xprofile
+echo "lxqt-panel &" >> /home/dxvk/.xprofile
+#THAT'S ALL FOLKS!!!
