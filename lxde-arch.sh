6a2759ae5fd3073491135360ab7956cba22afe91
document10 <popescul2222georgeteodor@gmail.com>
2022-02-20 21:43:52 +0200

    Create lxde-arch.sh

---
+++ lxde-arch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#must run as root
+pacman -Syu
+pacman -S xorg xorg-server xterm firefox lightdm  lightdm-gtk-greeter lightdm-gtk-greeter-settings archlinux-wallpaper avahi xdg-user-dirs xdg-utils gedit bluez bluez-utils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack sof-firmware lxde xdg-utils ttf-freefont blueman arc-solid-gtk-theme arc-gtk-theme arc-icon-theme libpulse libstatgrab libsysstat lm_sensors network-manager-applet oxygen-icons pavucontrol-qt picom
+systemctl enable lightdm
+systemctl enable bluetooth
+echo "compton -f &" >> /home/doc10/.xprofile
