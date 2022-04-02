a4a8dc41677108e1ec578431b129ae272320198c
document10 <popescul2222georgeteodor@gmail.com>
2022-04-02 13:11:24 +0300

    Update lxde-freebsd.sh

--- lxde-freebsd.sh
+++ lxde-freebsd.sh
@@ -1,6 +1,4 @@
-pkg install -y xf86-input-evdev xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xdg-user-dirs xdg-utils polkit polkit-gnome gtk-arc-themes lxde-meta bsdisks picom
+pkg install -y xf86-input-evdev xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xdg-user-dirs xdg-utils polkit polkit-gnome gtk-arc-themes lxde-meta bsdisks xfce4-wm
 sysrc lightdm_enable=YES
 sysrc webcamd_enable=YES
-echo "compton -f &" >> /home/dxvk/.xprofile
-echo "lxqt-panel &" >> /home/dxvk/.xprofile
 #THAT'S ALL FOLKS!!!
