55f86bba8c2153895e35e1c73f04a8faeb72ccc9
document10 <popescul2222georgeteodor@gmail.com>
2022-04-02 14:16:32 +0300

    Update lxqt-freebsd.sh

--- lxqt-freebsd.sh
+++ lxqt-freebsd.sh
@@ -1,5 +1,5 @@
-pkg install -y xf86-input-evdev xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xdg-user-dirs xdg-utils polkit polkit-gnome gtk-arc-themes lxqt bsdisks plasma5-kwin plasma5-systemsettings kwinft kf5-kwindowsystem konsole
-sysrc lightdm_enable=YES
+pkg install -y xf86-input-evdev xorg sddm xdg-user-dirs xdg-utils polkit polkit-gnome gtk-arc-themes lxqt bsdisks plasma5-kwin plasma5-systemsettings kwinft kf5-kwindowsystem konsole
+sysrc sddm_enable=YES
 sysrc webcamd_enable=YES
 echo "lxqt-panel &" >> /home/dxvk/.xprofile
 #THAT'S ALL FOLKS!!!
