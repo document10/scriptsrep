aad45893b290a3e9ff4403d8828ad6b64fb49d80
document10 <popescul2222georgeteodor@gmail.com>
2022-04-02 13:12:37 +0300

    Update lxde-arch.sh

--- lxde-arch.sh
+++ lxde-arch.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #must run as root
 pacman -Syu
-pacman -S xorg xorg-server xterm firefox lightdm  lightdm-gtk-greeter lightdm-gtk-greeter-settings archlinux-wallpaper avahi xdg-user-dirs xdg-utils gedit bluez bluez-utils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack sof-firmware lxde xdg-utils ttf-freefont blueman arc-solid-gtk-theme arc-gtk-theme arc-icon-theme libpulse libstatgrab libsysstat lm_sensors network-manager-applet oxygen-icons pavucontrol-qt picom
+pacman -S xorg xorg-server xterm firefox lightdm  lightdm-gtk-greeter lightdm-gtk-greeter-settings archlinux-wallpaper avahi xdg-user-dirs xdg-utils gedit bluez bluez-utils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack sof-firmware lxde xdg-utils ttf-freefont blueman arc-solid-gtk-theme arc-gtk-theme arc-icon-theme libpulse libstatgrab libsysstat lm_sensors network-manager-applet oxygen-icons pavucontrol-qt xfwm4
 systemctl enable lightdm
 systemctl enable bluetooth
-echo "compton -f &" >> /home/doc10/.xprofile
